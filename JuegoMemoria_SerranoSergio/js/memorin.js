eac20077c1467afe3e58894266c37c578fb26d54
sergio_serrano <sergioserranorodriguez9@gmail.com>
2022-10-24 17:15:47 +0200

    Prueba git

---
+++ JuegoMemoria_SerranoSergio/js/memorin.js
@@ -0,0 +1,83 @@
+class Tablero{
+    constructor(filas, columnas)
+    {
+    this.filas = filas;
+    this.columnas = columnas;
+    this.casillas = filas*columnas;
+    this.crearTablero();
+    }
+
+    //crea la array bidimensional del tablero
+    crearTablero()
+    {
+        this.tablero = [];
+    //si el numero de casillas es un numero par y no es igual a dos se creará la tabla   
+        if((this.casillas%2) == 0 && this.casillas != 2 && this.casillas > 0){
+            
+            for (let fila = 0; fila < this.filas; fila++) {
+                this.tablero[fila] = [];
+
+                for (let columna = 0; columna < this.columnas; columna++) {
+                    this.tablero[fila][columna] = '';
+                }
+            }
+
+        }
+    //si el número de casillas es 2 no se creará la tabla porque no tendría sentido iniciar el juego por tener una dificultad demasiado sencilla 
+        else if(this.casillas == 2){
+            alert('pon otro número que así sería demasiado fácil')
+        }
+    //  si el numero de casillas es negativo lanzará el siguiente error
+    else if(this.casillas < 0){
+        alert('El numero de casillas ha de ser mayor que dos (como mínimo)')
+    } 
+    // si el número de casillas es impar lanzará el siguiente error
+        else {
+            alert('El número de casillas ha de ser un número par')
+        }
+
+    }
+
+    //funcion que dibuja el tablero por pantalla
+    pintarTablero()
+    {
+        document.write('<h1>Juego de Memoria</h1>')
+        document.write('<table>');
+
+        for (let i = 0; i < this.filas; i++) {
+            document.write('<tr>');
+
+            for (let j = 0; j < this.columnas; j++) {
+                document.write(`<td>${this.tablero[i][j]}</td>`);
+            }
+
+            document.write('</tr>');
+        }
+        document.write('</table>');
+    }
+    
+}
+/*
+//clase referente al juego
+class Memorin{
+    constructor(filas, columnas, parejas) 
+    {
+        super(filas, columnas);
+        this.parejas = parejas;
+    }
+//funcion que creará las parejas
+    HacerParejas()
+    {
+        
+
+    }
+
+//funcion que colocará las parejas en el tablero
+    ponerParejas()
+    {
+
+    }
+}
+*/
+let tablero1 = new Tablero(prompt("¿Cuantas filas quieres tener en tu tablero"),prompt("¿Cuantas columnas quieres tener en tu tablero"));
+tablero1.pintarTablero();
