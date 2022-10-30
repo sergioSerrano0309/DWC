a1b5699f5296f5e1e75ef27a1fe4636a38081e6f
sergioSerrano0309 <103897663+sergioSerrano0309@users.noreply.github.com>
2022-10-30 13:02:46 +0100

    Add files via upload
    falta que guarde las nuevas columnas y las nuevas filas

--- JuegoMemoria_SerranoSergio_v2/js/memorin.js
+++ JuegoMemoria_SerranoSergio_v2/js/memorin.js
@@ -12,15 +12,14 @@ class Tablero{
     crearTablero()
     {
         this.tablero = [];
-    //si el numero de casillas es un numero par y no es igual a dos se creará la tabla   
-        
-        let sigue = true;
 
-        //while(sigue)
-        //{
+        //si el numero de casillas es un numero par y no es igual a dos se creará la tabla   
+        
+        let bucle = 0;
+        while(bucle == 0)
+        {
             if((this.casillas%2) == 0 && this.casillas != 2 && this.casillas > 0){
                 
-
                 for (let fila = 0; fila < this.filas; fila++) {
                     this.tablero[fila] = [];
 
@@ -29,54 +28,65 @@ class Tablero{
                     }
                 }
 
-                sigue = false;
+                bucle++;
 
             }
-        //si el número de casillas es 2 no se creará la tabla porque no tendría sentido iniciar el juego por tener una dificultad demasiado sencilla 
+            //si el número de casillas es 2 no se creará la tabla porque no tendría sentido iniciar el juego por tener una dificultad demasiado sencilla 
             else if(this.casillas == 2)
             {
                 alert('pon otro número que así sería demasiado fácil');
-               // filas = (prompt("¿Cuantas filas quieres tener en tu tablero"));
-               // columnas = (prompt("¿Cuantas columnas quieres tener en tu tablero"));
+               
+                let filas = prompt("¿Cuantas filas quieres tener en tu tablero");
+                let columnas = prompt("¿Cuantas columnas quieres tener en tu tablero");
+                let tablero1 = new Memorin(filas, columnas);
+                tablero1.pintarTablero();
+
             }
-        //  si el numero de casillas es negativo lanzará el siguiente error
+            //  si el numero de casillas es negativo lanzará el siguiente error
             else if(this.casillas <= 0)
             {
                 alert('El numero de casillas ha de ser mayor que 0');
-               // filas = (prompt("¿Cuantas filas quieres tener en tu tablero"));
-               // columnas = (prompt("¿Cuantas columnas quieres tener en tu tablero"));
+
+                let filas = prompt("¿Cuantas filas quieres tener en tu tablero");
+                let columnas = prompt("¿Cuantas columnas quieres tener en tu tablero");
+                let tablero1 = new Memorin(filas, columnas);
+                tablero1.pintarTablero();
+
             } 
-        // si el número de casillas es impar lanzará el siguiente error
+            // si el número de casillas es impar lanzará el siguiente error
             else if((this.casillas%2) != 0)
             {
                 alert('El número de casillas ha de ser un número par');
-                // filas = (prompt("¿Cuantas filas quieres tener en tu tablero"));
-               // columnas = (prompt("¿Cuantas columnas quieres tener en tu tablero"));
+
+                let filas = prompt("¿Cuantas filas quieres tener en tu tablero");
+                let columnas = prompt("¿Cuantas columnas quieres tener en tu tablero");
+                let tablero1 = new Memorin(filas, columnas);
+                tablero1.pintarTablero();
+
             }
-
-        //}
-
+        }
     }
-
-    //funcion que dibuja el tablero por pantalla
+        //funcion que dibuja el tablero por pantalla
     pintarTablero()
     {
-        document.write('<h1>Juego de Memoria</h1>')
-        document.write('<table>');
+            document.write('<h1>Juego de Memoria</h1>')
+            document.write('<table>');
 
-        for (let i = 0; i < this.filas; i++) {
-            document.write('<tr>');
+            for (let i = 0; i < this.filas; i++) {
+                document.write('<tr>');
 
-            for (let j = 0; j < this.columnas; j++) {
-                document.write(`<td>${this.tablero[i][j]}</td>`);
+                for (let j = 0; j < this.columnas; j++) {
+                    document.write(`<td>${this.tablero[i][j]}</td>`);
+                }
+
+                document.write('</tr>');
             }
-
-            document.write('</tr>');
-        }
-        document.write('</table>');
+            document.write('</table>');
     }
-    
+
 }
+
+
 
 //clase referente al juego
 
@@ -92,44 +102,38 @@ class Memorin extends Tablero{
 
     ponerParejas()
     {   
-        let lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let lista = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10];
+        let posicion = 0;
+        let contador = 0;
         let posFila;
         let posColumna;
-        let posicion = 0; 
-        let contadorParejas = 0;
-        let 
-           
-        while(posicion < this.casillas)
+
+        while (contador < this.casillas) {
+                
+            posFila = Math.floor(Math.random() * this.filas);
+            posColumna = Math.floor(Math.random() * this.columnas);
+
+            if (this.tablero[posFila][posColumna] == '' && posicion != 20)
             {
-                posFila = Math.floor(Math.random() * this.filas);
-                posColumna = Math.floor(Math.random() * this.columnas);
-
-                if (this.tablero[posFila][posColumna] == '') 
-                    {
-                        this.tablero[posFila][posColumna] = lista[posicion];     
-
-
-                        while(posicion < this.casillas)
-                        {
-                            posFila = Math.floor(Math.random() * this.filas);
-                            posColumna = Math.floor(Math.random() * this.columnas);
-
-                             if (this.tablero[posFila][posColumna] == '') 
-                                {
-                                    this.tablero[posFila][posColumna] = lista[posicion];     
-                                    posicion++;
-                       
-                                }             
-                            }
-                    }             
-                }
-           
-        posicion++; 
-    };
+                this.tablero[posFila][posColumna] = lista[posicion];
+                posicion++;
+                contador++;
+                    
+            }
+            else if (this.tablero[posFila][posColumna] == '' && posicion == 20)
+            {
+                posicion = 0;
+                this.tablero[posFila][posColumna] = lista[posicion];
+                posicion++;
+                contador++;
+            }
+                
+        }
+    }
+    
 }
 
-
-
-let tablero1 = new Memorin(prompt("¿Cuantas filas quieres tener en tu tablero"),prompt("¿Cuantas columnas quieres tener en tu tablero"));
-console.log(tablero1.tablero);
+let filas = prompt("¿Cuantas filas quieres tener en tu tablero");
+let columnas = prompt("¿Cuantas columnas quieres tener en tu tablero");
+let tablero1 = new Memorin(filas, columnas);        
 tablero1.pintarTablero();
