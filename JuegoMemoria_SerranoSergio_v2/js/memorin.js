16d2357e5e53aa87e835d85b2b8e0bc79517b63b
sergioSerrano0309 <103897663+sergioSerrano0309@users.noreply.github.com>
2022-10-30 16:12:15 +0100

    Add files via upload
    juego completado

--- JuegoMemoria_SerranoSergio_v2/js/memorin.js
+++ JuegoMemoria_SerranoSergio_v2/js/memorin.js
@@ -1,3 +1,4 @@
+document.write('<h1>Juego de Memoria</h1>')
 class Tablero{
     constructor(filas, columnas)
     {
@@ -13,12 +14,25 @@ class Tablero{
     {
         this.tablero = [];
 
+
+        //si el número de casillas es 2 no se creará la tabla porque no tendría sentido iniciar el juego por tener una dificultad demasiado sencilla.
+        //si el numero de casillas es negativo lanzará el siguiente error.
+        //si el número de casillas es impar lanzará el siguiente error.
+            if (this.casillas == 2 || (this.casillas%2) != 0 || this.filas <= 0 || this.columnas <= 0)
+            {
+                alert('Error en la inserción de valores.');
+
+                let filasNuevas = prompt("¿Cuantas filas quieres tener en tu tablero");
+                let columnasNuevas = prompt("¿Cuantas columnas quieres tener en tu tablero");
+
+                let tablero2 = new Memorin(filasNuevas, columnasNuevas);        
+                tablero2.pintarTablero();
+
+            }
+
         //si el numero de casillas es un numero par y no es igual a dos se creará la tabla   
-        
-        let bucle = 0;
-        while(bucle == 0)
-        {
-            if((this.casillas%2) == 0 && this.casillas != 2 && this.casillas > 0){
+                
+            else if((this.casillas%2) == 0 && this.casillas != 2 && this.filas > 0 && this.columnas > 0){
                 
                 for (let fila = 0; fila < this.filas; fila++) {
                     this.tablero[fila] = [];
@@ -28,48 +42,14 @@ class Tablero{
                     }
                 }
 
-                bucle++;
-
             }
-            //si el número de casillas es 2 no se creará la tabla porque no tendría sentido iniciar el juego por tener una dificultad demasiado sencilla 
-            else if(this.casillas == 2)
-            {
-                alert('pon otro número que así sería demasiado fácil');
-               
-                let filas = prompt("¿Cuantas filas quieres tener en tu tablero");
-                let columnas = prompt("¿Cuantas columnas quieres tener en tu tablero");
-                let tablero1 = new Memorin(filas, columnas);
-                tablero1.pintarTablero();
-
-            }
-            //  si el numero de casillas es negativo lanzará el siguiente error
-            else if(this.casillas <= 0)
-            {
-                alert('El numero de casillas ha de ser mayor que 0');
-
-                let filas = prompt("¿Cuantas filas quieres tener en tu tablero");
-                let columnas = prompt("¿Cuantas columnas quieres tener en tu tablero");
-                let tablero1 = new Memorin(filas, columnas);
-                tablero1.pintarTablero();
-
-            } 
-            // si el número de casillas es impar lanzará el siguiente error
-            else if((this.casillas%2) != 0)
-            {
-                alert('El número de casillas ha de ser un número par');
-
-                let filas = prompt("¿Cuantas filas quieres tener en tu tablero");
-                let columnas = prompt("¿Cuantas columnas quieres tener en tu tablero");
-                let tablero1 = new Memorin(filas, columnas);
-                tablero1.pintarTablero();
-
-            }
-        }
+            
     }
+    
         //funcion que dibuja el tablero por pantalla
     pintarTablero()
     {
-            document.write('<h1>Juego de Memoria</h1>')
+            
             document.write('<table>');
 
             for (let i = 0; i < this.filas; i++) {
