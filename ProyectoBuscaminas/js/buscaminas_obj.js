b109651daaf8963d0b8d9afb544ffacb98a750af
sergio_serrano <sergioserranorodriguez9@gmail.com>
2023-01-09 16:49:55 +0100

    creacion del proyecto memorin completo

--- ProyectoBuscaminas/js/buscaminas_obj.js
+++ ProyectoBuscaminas/js/buscaminas_obj.js
@@ -19,7 +19,7 @@ class Tablero {
         }
     }
 
-    pintarTablero() {
+    dibujarTableroHTML() {
         // Creamos el tablero en html
         document.write('<table>');
 
@@ -27,13 +27,39 @@ class Tablero {
             document.write('<tr>');
 
             for (let j = 0; j < this.columnas; j++) {
-                document.write(`<td>${this.arrayTablero[i][j]}</td>`);
+                document.write(`<td></td>`);
             }
 
             document.write('</tr>');
         }
         document.write('</table>');
     }
+
+    dibujarTableroDOM(){
+        // Creamos el tablero en DOM
+        let tabla = document.createElement('table');
+        let fila;
+        let columna;
+
+        for (let i = 0; i < this.filas; i++) {
+            fila = document.createElement('tr');
+            tabla.appendChild(fila);
+
+            for (let j = 0; j < this.columnas; j++) {
+                columna = document.createElement('td');
+                columna.id = `f${i}_c${j}`;
+                columna.dataset.fila = i;
+                columna.dataset.columna = j;
+                columna.dataset.despejado = false;
+                fila.appendChild(columna);
+            }
+        }
+
+        document.body.appendChild(tabla);
+    }
+
+    
+    
 
     modificarFilas(nuevasFilas) {
         // Modificar el número de filas y volver a crear el tablero con las filas nuevas
@@ -57,11 +83,12 @@ class Buscaminas extends Tablero {
         super(filas, columnas);
         this.numMinas = numMinas;
 
-        this.ponerMinas();
+        this.colocarMinas();
         this.colocarNumMinas();
+        this.dibujarTableroDOM();
     }
 
-    ponerMinas() {
+    colocarMinas() {
         let contadorMinas = 0;
         let posFila;
         let posColumna;
@@ -100,8 +127,176 @@ class Buscaminas extends Tablero {
             }
         }
     }
+
+    dibujarTableroDOM(){
+        super.dibujarTableroDOM();
+
+        let celda;
+
+        this.despejar = this.despejar.bind(this);
+        this.marcar = this.marcar.bind(this);
+
+        for (let i = 0; i < this.filas; i++) {
+            for (let j = 0; j < this.columnas; j++){
+                celda = document.getElementById(`f${i}_c${j}`);
+
+                celda.addEventListener('click', this.despejar);
+                celda.addEventListener('contextmenu', this.marcar);
+            }
+        }
+        console.log(this.arrayTablero);
+    }
+
+
+    ganar()
+    {
+        let contadorDeCasillas = 0;
+        let celda;
+        let despejado;
+
+        for (let i = 0; i < this.filas; i++) {
+            for (let j = 0; j < this.columnas; j++){
+                
+                celda = document.getElementById(`f${i}_c${j}`);
+                despejado = console.log(celda.getAttribute("data-despejado"));
+                if(despejado == true)
+                {
+                    contadorDeCasillas++
+                }
+            }
+        }
+        console.log(contadorDeCasillas)
+        if(contadorDeCasillas >= ((this.filas * this.columnas) - this. numMinas))
+        {
+            alert("Has ganado");
+        }
+    }
+
+    despejar(elEvento) {
+        let evento = elEvento || window.event;
+        let celda = evento.currentTarget;
+        
+        this.despejarCelda(celda);
+    }
+
+    despejarCelda(celda) {
+        let fila = parseInt(celda.dataset.fila);
+        let columna = parseInt(celda.dataset.columna);
+
+        // Marcar la celda despejada
+        celda.dataset.despejado = true;
+        celda.style.backgroundColor = "lightgrey";
+        celda.removeEventListener('click', this.despejar);
+        celda.removeEventListener('contextmenu', this.marcar);
+
+        let valorCelda = this.arrayTablero[fila][columna];
+        let esNumero = (valorCelda != 'MINA' && valorCelda != 0);
+        let esBomba = (valorCelda == 'MINA');
+        let esVacio = (valorCelda == 0);
+        let estaDespejado;
+        let bombaSeleccionadaMal;
+
+        let rutaBandera = "js/imgs/bandera.png";
+        
+        let arrayFilas;
+        let arrayColumnas; 
+        let celdaNueva;
+
+        if (esNumero) {
+            celda.innerHTML = valorCelda;
+            
+        } else if (esBomba) {
+            
+            arrayFilas = celda.parentNode.parentNode.childNodes;
+            for (let tr of arrayFilas) {
+                arrayColumnas = tr.childNodes;
+                for (let td of arrayColumnas){
+                    td.removeEventListener('click', this.despejar);
+                    td.removeEventListener('contextmenu', this.marcar);
+
+                    fila = td.dataset.fila;
+                    columna = td.dataset.columna;
+                    valorCelda = this.arrayTablero[fila][columna]
+                    if (td.lastChild != null){
+                        bombaSeleccionadaMal = (td.lastChild.src ==  rutaBandera && valorCelda != 'MINA');
+                    
+                        if (bombaSeleccionadaMal){
+                            td.lastChild.src = "";
+                            td.style.backgroundColor = 'red';
+                            td.innerHTML = valorCelda;
+                        } else if (valorCelda == 'MINA') {
+                            td.innerHTML = valorCelda;
+                        }
+                    } else if (valorCelda == 'MINA') {
+                            td.innerHTML = valorCelda;
+                    }
+                }
+            }
+            alert(`¡HAS PERDIDO!`);
+        }else if (esVacio) {
+
+            for (let cFila = fila - 1; cFila <= fila + 1; cFila++) {
+                if (cFila >= 0 && cFila < this.filas) {
+                    for (let cColumna = columna - 1; cColumna <= columna + 1; cColumna++) {
+                        if (cColumna >= 0 && cColumna < this.columnas) {
+                            celdaNueva = document.getElementById(`f${cFila}_c${cColumna}`)
+                            estaDespejado = (celdaNueva.dataset.despejado == 'true');
+                            if (!estaDespejado) {
+                                this.despejarCelda(celdaNueva);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        
+        this.ganar();
+    }
+
+    marcar(elEvento) {
+        document.oncontextmenu = function(){return false}
+        // Capturar el evento y el nodo que lo generó
+        let evento = elEvento || window.event;
+        let celda = evento.currentTarget;
+
+        // Crear el elemento img
+        let imagen = document.createElement('img');
+        imagen.style.height = "50px";
+
+        // Definir las rutas relativas donde se encuentran las imágenes
+        let rutaBandera = 'imgs/bandera.png';
+        let rutaInterrogante = 'imgs/interrogante.png';
+
+        // Definir la ruta relativa de la imagen en la celda seleccionada
+        let rutaImagen;
+
+        // Buscar la ruta relativa de la imagen para la celda seleccionada
+        if (celda.lastChild != null) {
+            rutaImagen = celda.lastChild.src.split('/').slice(-2).join('/');
+        }
+
+        // Comprobar imagen en la celda
+        let noHayImagen = (celda.lastChild == null);
+        let esBandera = (rutaImagen == rutaBandera);
+        let esInterrogante = (rutaImagen == rutaInterrogante);
+
+        // Marcar las celdas con la imagen adecuada
+        if (noHayImagen) {
+            celda.removeEventListener('click', this.despejar);
+            imagen.src = "imgs/bandera.png";
+            celda.appendChild(imagen);
+        } else if (esBandera) {
+            celda.addEventListener('click', this.despejar);
+            celda.lastChild.src = "imgs/interrogante.png";
+        } else if (esInterrogante) {
+            celda.removeChild(celda.lastChild);
+        }
+            
+    }
+
 }
 
-let buscaminas1 = new Buscaminas(5, 5, 5);
-console.log(buscaminas1.arrayTablero);
-buscaminas1.pintarTablero();
+window.onload = function() {
+    let buscaminas1 = new Buscaminas(5, 5, 5);
+}
